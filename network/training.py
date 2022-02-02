9391d64d189807f8063f5dfe4ae506bbd4c5a0d8
Luca Pegolotti <lpego@stanford.edu>
2022-02-02 10:57:24 -0800

    Add try except blocks for MPI unsupported

--- network/training.py
+++ network/training.py
@@ -99,11 +99,23 @@ def train_gnn_model(gnn_model, train, validation, optimizer_name, train_params,
         def weighted_mae(input, target):
             return mae(input, target)
 
-    gnn_model.module.set_normalization_coefs(coefs_dict)
-    train_sampler = DistributedSampler(train_dataset, num_replicas=dist.get_world_size(), rank=dist.get_rank())
+    try:
+        gnn_model.module.set_normalization_coefs(coefs_dict)
+        train_sampler = DistributedSampler(train_dataset, num_replicas=dist.get_world_size(), rank=dist.get_rank())
+    except AttributeError:
+        gnn_model.set_normalization_coefs(coefs_dict)
+        num_examples = len(train_dataset)
+        num_train = int(num_examples)
+        train_sampler = SubsetRandomSampler(torch.arange(num_train))
+
     train_dataloader = GraphDataLoader(train_dataset, sampler=train_sampler,
                                        batch_size=train_params['batch_size'],
                                        drop_last=False)
+    try:
+        print("my rank = %d, world = %d, train_dataloader_len = %d."
+              % (dist.get_rank(), dist.get_world_size(), len(train_dataloader)), flush=True)
+    except RuntimeError:
+        pass
 
     if optimizer_name == 'adam':
         optimizer = torch.optim.Adam(gnn_model.parameters(),
@@ -130,9 +142,6 @@ def train_gnn_model(gnn_model, train, validation, optimizer_name, train_params,
         # 200 is the maximum number of sigopt checkpoint
         chckp_epochs = list(np.floor(np.linspace(0, nepochs, 200)))
 
-
-    print("my rank = %d, world = %d, train_dataloader_len = %d."
-          % (dist.get_rank(), dist.get_world_size(), len(train_dataloader)), flush=True)
     for epoch in range(nepochs):
         global_loss, count, elapsed, global_mae, c_loss = evaluate_model(gnn_model, train_dataloader,
                                                                          mse, weighted_mae, optimizer,
@@ -184,22 +193,28 @@ def launch_training(dataset_json, optimizer_name, params_dict,
     now = datetime.now()
     train, validation, test = prepare_dataset(dataset_json)
     gnn_model = generate_gnn_model(params_dict)
-    gnn_model = torch.nn.parallel.DistributedDataParallel(gnn_model)
+    save_data = True
+    # check if MPI is supported
+    try:
+        gnn_model = torch.nn.parallel.DistributedDataParallel(gnn_model)
+        save_data = (dist.get_rank() == 0)
+    except RuntimeError:
+        pass
     folder = 'models/' + now.strftime("%d.%m.%Y_%H.%M.%S")
-    if dist.get_rank() == 0:
+    if save_data:
         pathlib.Path(folder).mkdir(parents=True, exist_ok=True)
         torch.save(gnn_model.state_dict(), folder + '/initial_gnn.pms')
     gnn_model, train_loader, loss, mae, coefs_dict, dataset = train_gnn_model(gnn_model,
-                                                                            train,
-                                                                            validation,
-                                                                            optimizer_name,
-                                                                            train_params,
-                                                                            checkpoint_fct,
-                                                                            dataset_params)
+                                                                              train,
+                                                                              validation,
+                                                                              optimizer_name,
+                                                                              train_params,
+                                                                              checkpoint_fct,
+                                                                              dataset_params)
 
     split = {'train': train.tolist(), 'validation': validation.tolist(), 'test': test.tolist()}
 
-    if dist.get_rank() == 0:
+    if save_data:
         torch.save(gnn_model.state_dict(), folder + '/trained_gnn.pms')
 
     dataset_params['split'] = split
@@ -220,14 +235,18 @@ def launch_training(dataset_json, optimizer_name, params_dict,
         print(obj)
         raise TypeError('Not serializable')
 
-    if dist.get_rank() == 0:
+    if save_data == 0:
         with open(folder + '/parameters.json', 'w') as outfile:
             json.dump(parameters, outfile, default=default)
     return gnn_model, loss, mae, dataset, coefs_dict, folder, parameters
 
 if __name__ == "__main__":
-    dist.init_process_group(backend='mpi')
-    print("my rank = %d, world = %d." % (dist.get_rank(), dist.get_world_size()), flush=True)
+    try:
+        dist.init_process_group(backend='mpi')
+        print("my rank = %d, world = %d." % (dist.get_rank(), dist.get_world_size()), flush=True)
+    except RuntimeError:
+        print("MPI not supported. Running serially.")
+
     dataset_json = json.load(open('training_dataset.json'))
 
     params_dict = {'infeat_nodes': 12,
@@ -241,14 +260,9 @@ if __name__ == "__main__":
     train_params = {'learning_rate': 0.008223127794360673,
                     'weight_decay': 0.36984122162067234,
                     'momentum': 0.0,
-<<<<<<< HEAD
                     'batch_size': 350,
                     'nepochs': 10,
                     'continuity_coeff': 10}
-=======
-                    'batch_size': 359,
-                    'nepochs': 30}
->>>>>>> main
     dataset_params = {'normalization': 'standard',
                       'rate_noise': 0.006,
                       'label_normalization': 'min_max'}
