Fix bug with scaling of mass continuity

network/graphnet.py
@@ -7,6 +7,7 @@ import torch.nn.functional as F
 import dgl.function as fn
 from torch.nn import Dropout
 import numpy as np
+import preprocessing as pp
 
 class MLP(Module):
     def __init__(self, in_feats, latent_space, out_feats, n_h_layers, normalize = True):
@@ -140,17 +141,36 @@ class GraphNet(Module):
         h = self.output(f)
         return {'h' : h}
 
-    def compute_continuity_loss(self, g, pred):
-        g.nodes['inner'].data['h'][:,1] = pred[:,1]
+    def compute_continuity_loss(self, g, pred, label_coefs, coefs_dict):
+        g.nodes['inner'].data['pred_q'] = torch.matmul(pred,
+                                          self.flowrate_selector)
+
+        # we have to bring the flowrate into the original units to compute
+        # mass loss
+        if label_coefs['normalization_type'] == 'min_max':
+            g.nodes['inner'].data['pred_q'] = label_coefs['min'][1] + \
+                                               g.nodes['inner'].data['pred_q'] * \
+                                              (label_coefs['max'][1] - label_coefs['min'][1])
+        elif label_coefs['normalization_type'] == 'standard':
+            g.nodes['inner'].data['pred_q'] = label_coefs['mean'][1] + \
+                                               g.nodes['inner'].data['pred_q'] * \
+                                              label_coefs['std'][1]
+        if coefs_dict['type'] == 'min_max':
+            g.nodes['inner'].data['pred_q'] = g.nodes['inner'].data['pred_q'] * \
+                                              float(coefs_dict['flowrate']['max'] - coefs_dict['min']['flowrate'])
+        elif coefs_dict['type'] == 'standard':
+            g.nodes['inner'].data['pred_q'] = g.nodes['inner'].data['pred_q'] * \
+                                              float(coefs_dict['flowrate']['std'])
+
         # this is kind of useless because pred should already be averaged
-        g.update_all(fn.copy_src('h', 'm'), fn.mean('m', 'average'),
+        g.update_all(fn.copy_src('pred_q', 'm'), fn.mean('m', 'average'),
                      etype='inner_to_macro')
-        g.nodes['macro'].data['average'] = g.nodes['macro'].data['average'][:,1]
-        g.update_all(fn.copy_src('average', 'm'), fn.mean('m', 'positve_flow'),
+        g.update_all(fn.copy_src('average', 'm'), fn.sum('m', 'positive_flow'),
                      etype='macro_to_junction_positive')
-        g.update_all(fn.copy_src('average', 'm'), fn.mean('m', 'negative_flow'),
+        g.update_all(fn.copy_src('average', 'm'), fn.sum('m', 'negative_flow'),
                      etype='macro_to_junction_negative')
-        diff = g.nodes['junction'].data['positve_flow'] - \
+
+        diff = g.nodes['junction'].data['positive_flow'] - \
                g.nodes['junction'].data['negative_flow']
 
         return torch.sum(torch.abs(diff))
@@ -195,5 +215,7 @@ class GraphNet(Module):
                       etype='macro_to_inner')
         g.update_all(fn.copy_e('correction', 'm'), fn.mean('m', 'correction'),
                      etype='macro_to_inner')
+        g.nodes['inner'].data['pred_q'] = g.nodes['inner'].data['pred_q'] + \
+                                          g.nodes['inner'].data['correction']
         return torch.cat((g.nodes['inner'].data['pred_p'],
                           g.nodes['inner'].data['pred_q']), dim=1)

network/training.py
@@ -22,6 +22,7 @@ import time
 import json
 import pathlib
 
+
 def mse(input, target):
     return ((input - target) ** 2).mean()
 
@@ -35,6 +36,8 @@ def generate_gnn_model(params_dict):
 
 def evaluate_model(gnn_model, train_dataloader, loss, metric = None,
                    optimizer = None, continuity_coeff = 0.0):
+    label_coefs = train_dataloader.dataloader.dataset.label_coefs
+    coefs_dict = train_dataloader.dataloader.dataset.coefs_dict
     global_loss = 0
     global_metric = 0
     count = 0
@@ -45,7 +48,12 @@ def evaluate_model(gnn_model, train_dataloader, loss, metric = None,
                          batched_graph.nodes['inner'].data['n_features'].float()).squeeze()
 
         if continuity_coeff > 1e-12:
-            c_loss = gnn_model.compute_continuity_loss(batched_graph, pred)
+            # real = gnn_model.compute_continuity_loss(batched_graph, batched_graph.nodes['inner'].data['n_labels'], label_coefs, coefs_dict)
+            # print(real)
+            try:
+                c_loss = gnn_model.module.compute_continuity_loss(batched_graph, pred, label_coefs, coefs_dict)
+            except AttributeError:
+                c_loss = gnn_model.compute_continuity_loss(batched_graph, pred, label_coefs, coefs_dict)
             loss_v = loss(pred, torch.reshape(batched_graph.nodes['inner'].data['n_labels'].float(),
                           pred.shape)) + c_loss * continuity_coeff
             c_loss_global = c_loss_global + c_loss.detach().numpy()
@@ -74,8 +82,11 @@ def evaluate_model(gnn_model, train_dataloader, loss, metric = None,
 def train_gnn_model(gnn_model, train, validation, optimizer_name, train_params,
                     checkpoint_fct = None, dataset_params = None):
     # we only compute the coefs_dict on the train_dataset
-    train_dataset, coefs_dict = pp.generate_dataset(train, dataset_params = dataset_params)
-    validation_dataset, _ = pp.generate_dataset(validation, coefs_dict, dataset_params)
+    train_dataset = pp.generate_dataset(train, dataset_params = dataset_params)
+    coefs_dict = train_dataset.coefs_dict
+    print('Dataset contains {:.0f} graphs'.format(len(train_dataset)))
+
+    validation_dataset = pp.generate_dataset(validation, coefs_dict, dataset_params)
 
     if dataset_params['label_normalization'] == 'min_max':
         def weighted_mae(input, target):
@@ -263,8 +274,8 @@ if __name__ == "__main__":
     train_params = {'learning_rate': 0.008223127794360673,
                     'weight_decay': 0.36984122162067234,
                     'momentum': 0.0,
-                    'batch_size': 350,
-                    'nepochs': 40,
+                    'batch_size': 1,
+                    'nepochs': 1,
                     'continuity_coeff': 0.1}
     dataset_params = {'normalization': 'standard',
                       'rate_noise': 0.006,