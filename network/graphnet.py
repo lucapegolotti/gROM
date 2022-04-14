Fix bug in computation of process stages (junction interactions were not computed) and improve rollout metrics

network/graphnet.py
@@ -9,6 +9,7 @@ import dgl.function as fn
 from torch.nn import Dropout
 import numpy as np
 import preprocessing as pp
+import time
 
 class MLP(Module):
     def __init__(self, in_feats, latent_space, out_feats, n_h_layers, normalize = True):
@@ -187,7 +188,7 @@ class GraphNet(Module):
 
     def process_nodes(self, nodes, processor):
         f1 = nodes.data['proc_node']
-        f2 = nodes.data['pe_sum']
+        f2 = nodes.data['pe_sum'] + nodes.data['pe_interaction']
         fin = nodes.data['inlet_info']
         fout = nodes.data['outlet_info']
         proc_node = processor(torch.cat((f1, f2, fin, fout), dim=1))
@@ -386,11 +387,11 @@ class GraphNet(Module):
 
             # compute junction-branch interactions
             g.apply_edges(pe_b2j, etype='branch_to_junction')
-            g.update_all(fn.copy_e('proc_edge', 'm'), fn.sum('m', 'pe_sum'),
+            g.update_all(fn.copy_e('proc_edge', 'm'), fn.sum('m', 'pe_interaction'),
                                    etype='branch_to_junction')
 
             g.apply_edges(pe_j2b, etype='junction_to_branch')
-            g.update_all(fn.copy_e('proc_edge', 'm'), fn.sum('m', 'pe_sum'),
+            g.update_all(fn.copy_e('proc_edge', 'm'), fn.sum('m', 'pe_interaction'),
                                    etype='junction_to_branch')
 
             # compute interactions in branches
@@ -404,7 +405,7 @@ class GraphNet(Module):
             g.apply_edges(pe_j2j, etype='junction_to_junction')
             # aggregate new edge features in nodes
             g.update_all(fn.copy_e('proc_edge', 'm'), fn.sum('m', 'pe_sum'),
-                                   etype='branch_to_branch')
+                                   etype='junction_to_junction')
             g.apply_nodes(pn_j, ntype='junction')
 
         g.apply_nodes(self.decode_branch, ntype='branch')

network/rollout.py
@@ -78,12 +78,9 @@ def rollout(gnn_model, params, dataset, index_graph, split, print_time = True):
     err_p_junction = 0
     err_q_junction = 0
 
+    norm_t = 0
     norm_p = 0
     norm_q = 0
-    norm_p_branch = 0
-    norm_q_branch = 0
-    norm_p_junction = 0
-    norm_q_junction = 0
     pred_states = [new_state]
     real_states = [new_state]
 
@@ -140,10 +137,14 @@ def rollout(gnn_model, params, dataset, index_graph, split, print_time = True):
 
         q_branch = dq_branch + prev_q
 
-        err_p_branch = err_p_branch + np.linalg.norm(p_branch - next_pressure_branch.detach().numpy().squeeze())**2
-        norm_p_branch = norm_p_branch + np.linalg.norm(next_pressure_branch.detach().numpy().squeeze())**2
-        err_q_branch = err_q_branch + np.linalg.norm(q_branch - next_flowrate_branch.detach().numpy().squeeze())**2
-        norm_q_branch = norm_q_branch + np.linalg.norm(next_flowrate_branch.detach().numpy().squeeze())**2
+        curr_err_p_branch = np.linalg.norm(p_branch - next_pressure_branch.detach().numpy().squeeze())**2
+        err_p_branch = err_p_branch + curr_err_p_branch
+        curr_norm_p_branch = np.linalg.norm(next_pressure_branch.detach().numpy().squeeze())**2
+        norm_t = norm_t + curr_norm_p_branch
+        curr_err_q_branch = np.linalg.norm(q_branch - next_flowrate_branch.detach().numpy().squeeze())**2
+        err_q_branch = err_q_branch + curr_err_q_branch
+        curr_norm_q_branch = np.linalg.norm(next_flowrate_branch.detach().numpy().squeeze())**2
+        norm_t = norm_t + curr_norm_q_branch
 
         dp_junction = bring_to_range_p(pred_junction[:,0].detach().numpy())
 
@@ -157,16 +158,17 @@ def rollout(gnn_model, params, dataset, index_graph, split, print_time = True):
 
         q_junction = dq_junction + prev_q
 
-        err_p_junction = err_p_junction + np.linalg.norm(p_junction - next_pressure_junction.detach().numpy().squeeze())**2
-        norm_p_junction = norm_p_junction + np.linalg.norm(next_pressure_junction.detach().numpy().squeeze())**2
-        err_q_junction = err_q_junction + np.linalg.norm(q_junction - next_flowrate_junction.detach().numpy().squeeze())**2
-        norm_q_junction = norm_q_junction + np.linalg.norm(next_flowrate_junction.detach().numpy().squeeze())**2
+        curr_err_p_junction = np.linalg.norm(p_junction - next_pressure_junction.detach().numpy().squeeze())**2
+        err_p_junction = err_p_junction + curr_err_p_junction
+        curr_norm_p_junction = np.linalg.norm(next_pressure_junction.detach().numpy().squeeze())**2
+        norm_t = norm_t + curr_norm_p_junction
+        curr_err_q_junction = np.linalg.norm(q_junction - next_flowrate_junction.detach().numpy().squeeze())**2
+        err_q_junction = err_q_junction + curr_err_q_junction
+        curr_norm_q_junction = np.linalg.norm(next_flowrate_junction.detach().numpy().squeeze())**2
+        norm_t = norm_t + curr_norm_q_junction
 
-        err_p = err_p + err_p_branch + err_p_junction
-        norm_p = norm_p + norm_p_branch + norm_p_junction
-
-        err_q = err_q + err_q_branch + err_q_junction
-        norm_q = norm_q + norm_q_branch + norm_q_junction
+        norm_p = curr_norm_p_branch + curr_norm_p_junction
+        norm_q = curr_norm_q_branch + curr_norm_q_junction
 
         pressure_dict_exact = {'branch': next_pressure_branch,
                                'junction': next_pressure_junction,
@@ -209,12 +211,14 @@ def rollout(gnn_model, params, dataset, index_graph, split, print_time = True):
         print('Rollout time = {:.2f} s for {:.0f} timesteps'.format(end - start,
                                                                     len(times)))
 
-    err_p_branch = np.sqrt(err_p_branch / norm_p_branch)
-    err_q_branch = np.sqrt(err_q_branch / norm_q_branch)
-    err_p_junction = np.sqrt(err_p_junction / norm_p_junction)
-    err_q_junction = np.sqrt(err_q_junction / norm_q_junction)
-    err_p = np.sqrt(err_p / norm_p)
-    err_q = np.sqrt(err_q / norm_q)
+    err_p = err_p_branch + err_p_junction
+    err_q = err_q_branch + err_q_junction
+    err_p_branch = err_p_branch
+    err_q_branch = err_q_branch
+    err_p_junction = err_p_junction
+    err_q_junction = err_q_junction
+    err_p = err_p
+    err_q = err_q
 
     errors = {'p_branch': err_p_branch,
               'q_branch': err_q_branch,
@@ -222,6 +226,9 @@ def rollout(gnn_model, params, dataset, index_graph, split, print_time = True):
               'q_junction': err_q_junction,
               'p': err_p,
               'q': err_q,
+              'norm_t': norm_t,
+              'norm_p': norm_p,
+              'norm_q': norm_q,
               'continuity': c_loss_total / total_flowrate}
 
     solutions = {'p_branch_real': pressures_branch_real,

network/training.py
@@ -68,14 +68,18 @@ def evaluate_model(gnn_model, train_dataloader, loss, metric = None,
         count = 0
         c_loss_global = 0
         for batched_graph in dataloader:
+            start = time.time()
             pred_branch, pred_junction = gnn_model(batched_graph, \
                                                    batched_graph.nodes['branch'].data['n_features'].float(), \
                                                    batched_graph.nodes['junction'].data['n_features'].float())
 
             pred_branch = pred_branch.squeeze()
             pred_junction = pred_junction.squeeze()
+            end = time.time()
+            # print('pred = {:.3f} s'.format(end - start))
 
             train_on_junctions = True
+            start = time.time()
 
             if train_on_junctions:
                 pred = torch.cat((pred_branch, pred_junction), 0)
@@ -96,6 +100,7 @@ def evaluate_model(gnn_model, train_dataloader, loss, metric = None,
                                                     pred_branch,
                                                     pred_junction,
                                                     label_coefs)
+
             except AttributeError:
                 if continuity_coeff > 1e-5:
                     c_loss = gnn_model.compute_continuity_loss(batched_graph, pred_branch, pred_junction, label_coefs, coefs_dict)
@@ -108,6 +113,10 @@ def evaluate_model(gnn_model, train_dataloader, loss, metric = None,
                                                     pred_junction,
                                                     label_coefs)
 
+            end = time.time()
+            # print('losses = {:.3f} s'.format(end - start))
+
+            start = time.time()
             loss_v = loss(pred, label)
             if continuity_coeff > 1e-5:
                 # real = gnn_model.compute_continuity_loss(batched_graph, batched_graph.nodes['inner'].data['n_labels'], label_coefs, coefs_dict)
@@ -125,10 +134,16 @@ def evaluate_model(gnn_model, train_dataloader, loss, metric = None,
 
                 global_metric = global_metric + metric_v.detach().numpy()
 
+            end = time.time()
+            # print('boh = {:.3f} s'.format(end - start))
+
+            start = time.time()
             if c_optimizer != None:
                optimizer.zero_grad()
                loss_v.backward()
                optimizer.step()
+            end = time.time()
+            # print('optimizer = {:.3f} s'.format(end - start))
             count = count + 1
 
         return {'global_loss': global_loss, 'count': count,
@@ -279,25 +294,26 @@ def train_gnn_model(gnn_model, optimizer_name, parameters,
                                           print_time = False)
 
                 error_branches_train = error_branches_train + \
-                                       np.sqrt(errors['p_branch']**2 + errors['q_branch']**2)
+                                       (errors['p_branch'] + errors['q_branch']) / errors['norm_t']
 
                 error_junctions_train = error_junctions_train + \
-                                        np.sqrt(errors['p_junction']**2 + errors['q_junction']**2)
+                                        (errors['p_junction'] + errors['q_junction']) / errors['norm_t']
 
                 error_global_train = error_global_train + \
-                                     np.sqrt(errors['p']**2 + errors['q']**2)
+                                     (errors['p'] + errors['q']) / errors['norm_t']
 
             error_branches_train = error_branches_train / nrollout
             error_junctions_train = error_junctions_train / nrollout
             error_global_train = error_global_train / nrollout
 
             history['train_rollout_error'][0].append(epoch)
-            history['train_rollout_error'][1].append(error_global_train)
+            history['train_rollout_error'][1].append(np.sqrt(error_global_train))
 
             msg = 'Rollout train: '
             msg = msg + 'rollout_error_branch = {:.5e} '.format(error_branches_train)
             msg = msg + 'rollout_error_junctions = {:.5e} '.format(error_junctions_train)
             msg = msg + 'rollout_error_global = {:.5e} '.format(error_global_train)
+            msg = msg + 'sqrt = {:.5e} '.format(np.sqrt(error_global_train))
 
             print(msg)
 
@@ -314,23 +330,26 @@ def train_gnn_model(gnn_model, optimizer_name, parameters,
                                           print_time = False)
 
                 error_branches_test = error_branches_test + \
-                                       np.sqrt(errors['p_branch']**2 + errors['q_branch']**2)
+                                      (errors['p_branch'] + errors['q_branch']) / errors['norm_t']
 
                 error_junctions_test = error_junctions_test + \
-                                        np.sqrt(errors['p_junction']**2 + errors['q_junction']**2)
+                                       (errors['p_junction'] + errors['q_junction']) / errors['norm_t']
 
                 error_global_test = error_global_test + \
-                                     np.sqrt(errors['p']**2 + errors['q']**2)
+                                    (errors['p'] + errors['q']) / errors['norm_t']
 
+            error_branches_test = error_branches_test / nrollout
+            error_junctions_test = error_junctions_test / nrollout
             error_global_test = error_global_test / nrollout
 
             history['test_rollout_error'][0].append(epoch)
-            history['test_rollout_error'][1].append(error_global_test)
+            history['test_rollout_error'][1].append(np.sqrt(error_global_test))
 
             msg = 'Rollout test: '
             msg = msg + 'rollout_error_branch = {:.5e} '.format(error_branches_test)
             msg = msg + 'rollout_error_junctions = {:.5e} '.format(error_junctions_test)
             msg = msg + 'rollout_error_global = {:.5e} '.format(error_global_test)
+            msg = msg + 'sqrt = {:.5e} '.format(np.sqrt(error_global_test))
 
             print(msg)
 
@@ -442,7 +461,7 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='gROM SimVascular Project.')
 
     parser.add_argument('--bs', help='batch size', type=int, default=100)
-    parser.add_argument('--epochs', help='total number of epochs', type=int, default=1)
+    parser.add_argument('--epochs', help='total number of epochs', type=int, default=200)
     parser.add_argument('--lr_decay', help='learning rate decay', type=float, default=0.1)
     parser.add_argument('--lr', help='learning rate', type=float, default=0.008)
     parser.add_argument('--rate_noise', help='rate noise', type=float, default=600)